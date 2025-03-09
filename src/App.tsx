97fe75c6db7690ae79282fa939c8402cdb60acba
amami1625 <amami_gt0403@yahoo.co.jp>
2025-03-10 02:22:11 +0900

    ルーティングを設定

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
-import Header from './components/Header'
+import Layout from './components/common/Layout'
+import Daily from './components/pages/Daily'
+import Monthly from './components/pages/Monthly'
 
 function App() {
 
   return (
     <>
-      <Header />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Layout />}>
+            <Route index element={<Daily />}/>
+            <Route path='/monthly' element={<Monthly />}/>
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
