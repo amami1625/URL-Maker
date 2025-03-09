3e6fed3d95396bc1a2b944c5fae45fea4bd5ea1b
amami1625 <amami_gt0403@yahoo.co.jp>
2025-03-10 02:51:25 +0900

    Daily へステートとハンドラー、コンポーネントを表示する処理を追加

--- src/components/pages/Daily.tsx
+++ src/components/pages/Daily.tsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import { useState } from "react";
+import styled from "styled-components";
+
+const Button = styled.button``;
+const Ul = styled.ul``;
+const Li = styled.li``;
+
+type UrlList = {
+  name: string;
+  url: string;
+};
 
 const Daily = () => {
-  return (
-    <div>Daily</div>
-  )
-}
+  const [urlList, setUrlList] = useState<UrlList[]>([]);
 
-export default Daily
+  const clickHandler = () => {
+    const baseUrl: string[] = import.meta.env.VITE_URL_LISTS.split(",");
+    const nameList: string[] = import.meta.env.VITE_NAMES.split(",");
+    const data = baseUrl.map((url, i) => ({
+      name: nameList[i],
+      url,
+    }));
+    setUrlList(data);
+  };
+  return (
+    <div>
+      <Button onClick={clickHandler}>当日分のURLを取得</Button>
+      <Ul>
+        {urlList.map((item) => (
+          <Li key={item.url}>
+            <a href={item.url} target="_blank">{item.name}</a>
+          </Li>
+        ))}
+      </Ul>
+    </div>
+  );
+};
+
+export default Daily;
