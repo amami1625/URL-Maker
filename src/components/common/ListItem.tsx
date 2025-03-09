221134218df7929d4b8b911dcfac4d70b46b9c16
amami1625 <amami_gt0403@yahoo.co.jp>
2025-03-10 04:09:36 +0900

    ListItem のスタイルを変更

--- src/components/common/ListItem.tsx
+++ src/components/common/ListItem.tsx
@@ -1,19 +1,37 @@
 import styled from "styled-components";
 import { UrlList } from "../../types/type";
 
-const Li = styled.li``;
+const Li = styled.li`
+  padding: 8px;
+  border-bottom: dashed 2px #6594e0;
+  // border-bottom: 1px solid #ccc;
+`;
+
+const A = styled.a`
+  font-size: 20px;
+  color: #6594e0;
+  &:hover {
+    opacity: 0.7;
+  }
+`;
+
+const Span = styled.span`
+  display: block;
+  font-size: 10px;
+`;
 
 type ListItemProps = {
   item: UrlList;
   copy: (e: React.MouseEvent<HTMLAnchorElement>) => void;
-}
+};
 
 const ListItem = ({ item, copy }: ListItemProps) => {
   return (
     <Li>
-      <a onClick={copy} href={item.url} target="_blank">
+      <A onClick={copy} href={item.url} target="_blank">
         {item.name}
-      </a>
+        <Span>{item.url}</Span>
+      </A>
     </Li>
   );
 };
