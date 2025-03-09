87fde03e1878e4d245d19fe4a082c2a17bede611
amami1625 <amami_gt0403@yahoo.co.jp>
2025-03-10 03:42:00 +0900

    Daily のボタンのスタイルを変更

--- src/components/pages/Daily.tsx
+++ src/components/pages/Daily.tsx
@@ -1,7 +1,31 @@
 import React, { useState } from "react";
 import styled from "styled-components";
 
-const Button = styled.button``;
+const Button = styled.button`
+  border-radius: 5px;
+  position: relative;
+  display: flex;
+  margin-left: auto;
+  max-width: 220px;
+  padding: 10px 16px;
+  color: #fff;
+  transition: 0.3s ease-in-out;
+  font-weight: 600;
+  background: rgb(149, 202, 252);
+  background: linear-gradient(
+    270deg,
+    rgba(149, 202, 252, 1) 0%,
+    rgba(107, 182, 255, 1) 100%
+  );
+  &:hover {
+    background: rgb(117, 188, 255);
+    background: linear-gradient(
+      270deg,
+      rgba(117, 188, 255, 1) 0%,
+      rgba(62, 159, 252, 1) 100%
+    );
+  }
+`;
 const Ul = styled.ul``;
 const Li = styled.li``;
 
@@ -18,8 +42,8 @@ const Daily = () => {
     const nameList: string[] = import.meta.env.VITE_NAMES.split(",");
     const y = String(new Date().getFullYear());
     const m = String(new Date().getMonth() + 1).padStart(2, "0");
-    const d = String(new Date().getDate() -1).padStart(2, "0");
-    const param = `${y}${m}${d}/`
+    const d = String(new Date().getDate() - 1).padStart(2, "0");
+    const param = `${y}${m}${d}/`;
     const data = baseUrl.map((url, i) => ({
       name: nameList[i],
       url: url + param,
