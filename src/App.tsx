c7b2820e45b1dcf76cff9a155b8baf9f0a7fd528
amami1625 <amami_gt0403@yahoo.co.jp>
2025-03-10 03:11:36 +0900

    CSS をリセット

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,21 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import './App.css'
-import Layout from './components/common/Layout'
-import Daily from './components/pages/Daily'
-import Monthly from './components/pages/Monthly'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Layout from "./components/common/Layout";
+import Daily from "./components/pages/Daily";
+import Monthly from "./components/pages/Monthly";
 
 function App() {
-
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Layout />}>
-            <Route index element={<Daily />}/>
-            <Route path='/monthly' element={<Monthly />}/>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Daily />} />
+            <Route path="/monthly" element={<Monthly />} />
           </Route>
         </Routes>
       </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
