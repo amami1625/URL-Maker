3d9173b3725af2647dad970ecaba22322024ff80
amami1625 <amami_gt0403@yahoo.co.jp>
2025-03-10 03:06:48 +0900

    タイポを修正

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,7 +10,7 @@ const Header = () => {
     <HeaderElm>
       <h1>URLメーカー(β)</h1>
       <Ul>
-        <Li><Link to="/">当日</Link></Li>
+        <Li><Link to="/">前日</Link></Li>
         <Li><Link to="/monthly">月次</Link></Li>
       </Ul>
     </HeaderElm>
