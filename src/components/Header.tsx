09dd5a3f54ab6f34f4f5999f9c3861f7a8ab5f24
amami1625 <amami_gt0403@yahoo.co.jp>
2025-03-10 02:49:44 +0900

    Header にリンクを設定

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,18 @@
+import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 const HeaderElm = styled.header``;
+const Ul = styled.ul``;
+const Li = styled.li``;
 
 const Header = () => {
   return (
     <HeaderElm>
       <h1>URLメーカー(β)</h1>
+      <Ul>
+        <Li><Link to="/">当日</Link></Li>
+        <Li><Link to="/monthly">月次</Link></Li>
+      </Ul>
     </HeaderElm>
   );
 };
