64560bc35175276bef9ed0135533d3eec8db85aa
amami1625 <amami_gt0403@yahoo.co.jp>
2025-03-10 03:27:41 +0900

    Header のスタイルを変更

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,29 @@
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 
-const HeaderElm = styled.header``;
-const Ul = styled.ul``;
-const Li = styled.li``;
+const HeaderElm = styled.header`
+  text-align: center;
+  padding: 8px 0;
+  background-color: oklch(0.746 0.16 232.661);
+`;
+const Ul = styled.ul`;
+`;
+const Li = styled.li`
+  display: inline-block;
+  padding: 4px;
+  `;
 
 const Header = () => {
   return (
     <HeaderElm>
       <h1>URLメーカー(β)</h1>
       <Ul>
-        <Li><Link to="/">前日</Link></Li>
-        <Li><Link to="/monthly">月次</Link></Li>
+        <Li>
+          <Link to="/">前日</Link>
+        </Li>
+        <Li>
+          <Link to="/monthly">月次</Link>
+        </Li>
       </Ul>
     </HeaderElm>
   );
