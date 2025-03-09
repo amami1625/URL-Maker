f77ec542dbd3748ce077f94e34cc9bda31ac06a5
amami1625 <amami_gt0403@yahoo.co.jp>
2025-03-10 02:12:47 +0900

    Header コンポーネントを作成

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const HeaderElm = styled.header``;
+
+const Header = () => {
+  return (
+    <HeaderElm>
+      <h1>URLメーカー(β)</h1>
+    </HeaderElm>
+  );
+};
+
+export default Header;
